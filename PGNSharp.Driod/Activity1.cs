b9ec14f9a10113c2437bdafb386b3a2d8347f1ba
Kevin Bost <kitokeboo@yahoo.com>
2014-03-18 22:16:16 -0700

    Starting work on the andriod app. Still can't get it to layout like a grid though.... Grrrr.....

---
+++ PGNSharp.Driod/Activity1.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using System.Text;
+using Android.App;
+using Android.OS;
+using Android.Widget;
+using PGNSharp.Core;
+using PGNSharp.Driod.Resources;
+
+namespace PGNSharp.Driod
+{
+    [Activity(Label = "PGNSharp.Driod", MainLauncher = true, Icon = "@drawable/icon")]
+    public class Activity1 : Activity
+    {
+        private Game _game;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            using (var stream = new MemoryStream(Encoding.ASCII.GetBytes(TestData.TestGame.PGN)))
+            {
+                _game = Game.Load(stream);
+            }
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Id.gridView1);
+
+            var gridView = FindViewById<GridView>(Resource.Id.gridView1);
+            gridView.Adapter = new BoardAdapter(this, _game);
+            //ListView.Adapter = new BoardAdapter(this, _game);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+        }
+    }
+}
+
---
+++ PGNSharp.Driod/Resources/BoardAdapter.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Android.App;
+using Android.Views;
+using Android.Widget;
+using PGNSharp.Core;
+
+namespace PGNSharp.Driod.Resources
+{
+    public class BoardAdapter : BaseAdapter<string>
+    {
+        private readonly Game _game;
+        private readonly Activity _context;
+
+        public BoardAdapter(Activity context, Game game)
+        {
+            _context = context;
+            _game = game;
+        }
+
+        public override long GetItemId(int position)
+        {
+            return position;
+        }
+
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+            if (convertView == null)
+                convertView = _context.LayoutInflater.Inflate(Resource.Layout.board_cell, null);
+
+            var textView = convertView.FindViewById<TextView>(Resource.Id.cellText);
+            textView.Text = BoardCharacterFromPosition(position);
+            return convertView;
+        }
+
+        public override int Count
+        {
+            get { return 64; }
+        }
+
+        public override string this[int position]
+        {
+            get { return BoardCharacterFromPosition(position); }
+        }
+
+        private string BoardCharacterFromPosition(int position)
+        {
+            var file = (char)('a' + position % 8);
+            int rank = position / 8 + 1;
+            var piece = _game.GetPiece(new Location(file, rank));
+            if (piece == null)
+                return "-";
+            switch (piece.Type)
+            {
+                case PieceType.Pawn:
+                    return piece.Color == PieceColor.White ? "P " : "p ";
+                case PieceType.Rook:
+                    return piece.Color == PieceColor.White ? "R " : "r ";
+                case PieceType.Knight:
+                    return piece.Color == PieceColor.White ? "N " : "n ";
+                case PieceType.Bishop:
+                    return piece.Color == PieceColor.White ? "B " : "b ";
+                case PieceType.Queen:
+                    return piece.Color == PieceColor.White ? "Q " : "q ";
+                case PieceType.King:
+                    return piece.Color == PieceColor.White ? "K " : "k ";
+            }
+            throw new InvalidOperationException();
+        }
+    }
+}
