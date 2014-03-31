11c3cc42dc98b711447b63eea8b04113834ad082
Kevin Bost <kitokeboo@yahoo.com>
2014-03-30 22:58:28 -0700

    Adding WP8 version of the app. Took less than 45 mins from start to here.

---
+++ PGNSharp.WP8/Board.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Controls;
+using PGNSharp.Core;
+
+namespace PGNSharp.WP8
+{
+    public partial class Board
+    {
+        private readonly TextBlock[,] _boardSpaces = new TextBlock[8,8];
+
+        public Board()
+        {
+            InitializeComponent();
+            foreach (var grid in LayoutRoot.Children.Cast<Grid>())
+            {
+                //adjust for a1 being in the botton left
+                int row = 7 - Grid.GetRow(grid);
+                int column = Grid.GetColumn(grid);
+
+                _boardSpaces[column, row] = grid.Children.Cast<TextBlock>().Single();
+            }
+        }
+
+        public void SetPieces(Game game)
+        {
+            if (game == null) throw new ArgumentNullException("game");
+            for (int rank = 1; rank <= 8; rank++)
+            {
+                for (char file = 'a'; file <= 'h'; file++)
+                {
+                    Piece piece = game.GetPiece(new Location(file, rank));
+                    _boardSpaces[file - 'a', rank - 1].Text = piece != null ? AsciiPiece.GetCharForPiece(piece).ToString() : "";
+                }
+            }
+        }
+    }
+}
---
+++ PGNSharp.WP8/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using PGNSharp.Core;
+using TestData;
+
+namespace PGNSharp.WP8
+{
+    public partial class MainPage
+    {
+        private readonly Game _game;
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            _game = Game.Load(TestGame.PGN);
+            Board.SetPieces(_game);
+        }
+
+        private void NextMoveOnClick(object sender, RoutedEventArgs e)
+        {
+            _game.NextMove();
+            Board.SetPieces(_game);
+        }
+
+        private void ResetOnClick(object sender, RoutedEventArgs e)
+        {
+            _game.ResetMoves();
+            Board.SetPieces(_game);
+        }
+    }
+}
